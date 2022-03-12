1019598f953539504d798ec98d8c847f3fd7e0ae
gk2803 <georkyri7@gmail.com>
2022-03-12 21:50:55 +0200

    επιπλέον σχολιασμός προτύπου &  crossover function

--- draft.py
+++ draft.py
@@ -16,12 +16,13 @@ def n_elements_sum(l,n):
 #κατασκευη πιθανων λυσεων 
 
 #Ν αριθμος πιθανων λυσεων
-N = 10
-bits = 6
+N = 6
+bits = 5
 
 #προβλημα
 def foo(x):
     return x**2
+
 
 #αρχικοποίηση λίστας με τυχαίους αριθμούς
 a = []
@@ -29,27 +30,35 @@ for i in range(N):
      a.append(random.randint(1,31))
 
 
-#αξιολογηση
+#αξιολογηση (υπολογισμός τιμών μέσω της συνάρτησης) (fitness)
+#ο πινακας t περιεχει τις "λυσεις" των αρχικοποιημενων τυχαίων αριθμων
 t=[]
 for i in range(N):
     t.append(foo(a[i]))
 
- 
+# τα στοιχεία του πίνακα f αποτελούν την πιθανότητα επιλογής των στοιχείων του πίνακα t
+# η πιθανότητα κάθε χρωμοσώματος υπολογίζεται διαιρώντας την αξιολόγηση (fitness) με τον συνολικό
+# αριθμό αξιολογήσεων s = sum(t)
 f=[]
-#αθροισμα αξιολογησεων και εκχωρηση στην μεταβλητη s
 s= sum(t)
-#πιθανοτητα επιλογης του κανε χρωμοσωματος(λυσης)
 for i  in range(N):
     f.append(t[i]/s)
 
-average = s/N
-max = max(t)
-#υπολογισμος αθροιστικης ικανοτητας
+# average = s/N τα average και max δεν χρειάζονται αλλά αποτελούν ένα ένδιαφέρον στατιστικό
+# max = max(t)
+
+# βήμα αθροιστικής πιθανότητας όπου θα μας βοηθήσει
+# στην αναπαράσταση των πιθανοτήτων σε μια ρουλέτα
+# υπολογισμος αθροιστικης πιθανότητα: 
+# q[0] = f[0]
+# q[1] = f[0] + f[1]
+# q[2] = f[0] + f[1] + f[2] κ.ο.κ
+# όπου f[i] η πιθανότητα του χρωμοσώματος i
 q=[]
 for i in range(N):
     q.append(n_elements_sum(f,i))
 
-#επιλογη ρουλετας 
+#επιλογη ρουλετας - διασταύρωση
 select = [0 for _ in range(N)]
 
 for i in range(N):
@@ -59,16 +68,38 @@ for i in range(N):
             select[j]+=1
             break
 
-#διασταυρωση μονου σημειου
-#χωριζουμε τον πληθυσμο σε ζευγη με βαση τη σειρα επιλογης
-#
+
 
 last_list=[]
 for i in range(N):
     last_list.append((a[i],f[i],select[i]))
     
+#διασταυρωση μονου σημειου (one point crossover)
+#συναρτηση που θα χρησιμοποιηθει ΚΑΤΑ τη διαδικασια της επιλογης
+def crossover(parent1, parent2):
+    
+    # ρουλέτα για επιλογή σημείου bits -1 καθως τα σημεια στα οποια 
+    # μπορει να χωριστει το binary ειναι bits-1 
+    # σσ. bits = ο αριθμος των ψηφιων της δυαδικης συμβολοσειρας π.χ. ο αριθμος 31 σε binary ειναι 11111
+    
+    r = random.random()
+    for i in range(bits-1):
+        if (r<=i/(bits-1)):
+            position = i
+            break
+    child1 = parent1[:position]+parent2[position:]
+    child2 = parent2[:position]+parent1[position:]
+    print("το σημείο τομής είναι",position)
+    return child1,child2
+    
+
+print(crossover("01234","99912"))
+    
+
+
+
 
 #last_list.sort(key=lambda y: y[2])
 
 for i in range(N):
-    print(last_list[i])
+    print(f"chromosome: {last_list[i][0]}, probability: {last_list[i][1]:.2f}, roulette-selection: {last_list[i][2]}")
