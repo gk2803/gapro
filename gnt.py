c81de0bc17ac47f94b6741eab60581d2ebd2bac1
gk2803 <georkyri7@gmail.com>
2022-03-14 10:43:36 +0200

    double constructor for random values

--- gnt.py
+++ gnt.py
@@ -1,5 +1,6 @@
+from pickle import POP
 import random 
-
+import time
 POP_SIZE = 10
 #megistos arithmos bits simfwna me to pedio orismou 
 bits = 5
@@ -28,32 +29,46 @@ class Chromosome:
         self.genes = []
         self.prob = 0
         self.qprob = 0
-        self.x = random.randint(bounds[0][0],bounds[0][1])
-        self.y = random.randint(bounds[1][0],bounds[1][1])
-        self.z = random.randint(bounds[2][0],bounds[2][1])
+        x = random.randint(bounds[0][0],bounds[0][1])
+        y = random.randint(bounds[1][0],bounds[1][1])
+        z = random.randint(bounds[2][0],bounds[2][1])
         # προσθετει μηδενικα μπροστα απο καθε gene οπου ο αριθμος των δυαδικων με .zfill
         # του ψηφιων ειναι μικροτερος απο τον μεγιστο αριθμο bits εντος του πεδιου ορισμου (7) 
         
-        self.genes.append(list(bin(self.x)[2:].zfill(bits)))
-        self.genes.append(list(bin(self.y)[2:].zfill(bits)))
-        self.genes.append(list(bin(self.z)[2:].zfill(bits)))
+        self.genes.append(list(bin(x)[2:].zfill(bits)))
+        self.genes.append(list(bin(y)[2:].zfill(bits)))
+        self.genes.append(list(bin(z)[2:].zfill(bits)))
         #fitness 
-        self.fitness = objective_function(self.get_real_genes())
-    
+        self.fitness = objective_function(self.get_int())
+ 
 
-    def get_real_genes(self):
-        return [self.x,self.y,self.z]
-    
-    
-    
+    def get_int(self):
+        t = list()
+        for gene in self.genes:
+            t.append(int(''.join(gene),2))
+        return t
+   
+    def __str__(self):
+        s = "" 
+        t = self.get_int()
+        for gene in self.genes:
+            s += f"[{''.join(gene)}]"
+        s+= f" x={t[0]}, y={t[1]}, z={t[2]}"
+        return s
+        
+    def get_genes(self):
+        return self.genes
 
+    def set_genes(self,t):
+        self.genes = t
 
+ #TODO sdsd
 class Population:
     def __init__(self):
         
         self.pop = []
         self.sum = 0
-        self.q = 0
+        q = 0
         for _ in range(POP_SIZE):
             s = Chromosome()
             self.sum += s.fitness
@@ -61,25 +76,65 @@ class Population:
         #init probabilities, quantitive probabilities
         for elem in self.pop:
             elem.prob = elem.fitness/self.sum
-            self.q += elem.prob
-            elem.qprob += self.q
+            q += elem.prob
+            elem.qprob = q
         
+    def get_pop(self):
+        return self.pop
 
-        
     
+    #TODO  sadasdadads
     def __str__(self):
         s = ""
         for elem in self.pop:
-            s+=f'x: {elem.x}, y: {elem.y}, z: {elem.z} fitness: {elem.fitness} prob: {elem.prob} qprob: {elem.qprob}\n'
+            s+=f' {elem.get_int()} fitness: {elem.fitness} prob: {elem.prob:.2f} qprob: {elem.qprob:.2f}\n'
         return s
     
 
 
 
-class Genetic_Algorithm:
-    def __init__(self):
-        pass
-        
-
-p = Population()
-print(p)
+#class Genetic_Algorithm:
+#    #επιδρα πανω στον υπαρχον πληθυσμο και κραταει μονο 
+#    #τα χρωμοσωματα που περνανε το σταδιο της επιλογης
+#    @staticmethod
+#    def selection(pop: Population):
+#        # t λιστα χρωμοσωμάτων
+#        t = pop.get_pop()
+#        for i in range(POP_SIZE):
+#            r = random.random()
+#            for index,elem in enumerate(pop.get_pop(),start=0):
+#                if r<=elem.qprob:
+#                    t[index] = elem
+#                    break
+#        return t 
+#                
+#        
+#
+#    @staticmethod
+#    def crossover(pop: Population):
+#        pop = pop.get_pop()
+#        print(len(pop))
+#        for i in range(int(POP_SIZE/2)):
+#            p1 = pop[2*i-1].get_genes()
+#            
+#            p2 = pop[2*i].get_genes()
+#            r = random.random()
+#            for i in range(bits-1):
+#                if (r<=i/(bits-1)):
+#                    index = i
+#            c1 = [p1[0][:index] +p2[0][index:],p1[1][:index] + p2[1][index:], p1[2][:index] +p2[2][index:]]
+#            c2 = [p2[0][:index] +p1[0][index:],p2[1][:index] + p1[1][index:], p2[2][:index] +p1[2][index:]]
+#            pop[2*i-1].set_genes(c1)
+#            pop[2*i].set_genes(c2)
+#        
+#
+#        
+#
+#
+#pop = Population()
+#print(pop)
+#g=Genetic_Algorithm()
+#print(g.selection(pop))
+#
+##g.crossover(pop)
+##print(pop)
