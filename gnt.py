40bf4fc57296cc487652f704f8ca914c94d81005
gk2803 <georkyri7@gmail.com>
2022-05-22 17:23:20 +0300

    before scaling

--- gnt.py
+++ gnt.py
@@ -1,75 +1,72 @@
 from operator import attrgetter
-import tkinter as tk
 import random
-import time
-import matplotlib.pyplot as plt
-import sys
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib import pyplot 
-from matplotlib.animation import FuncAnimation
-from itertools import count
-
-
-
 
 
 class Chromosome:
-    def __init__(self, genes: list,bounds, prob=0, qprob=0):
+    def __init__(
+        self,
+        genes: list,
+        bounds: list,
+        objective_function: callable,
+        prob=0,
+        qprob=0,
+    ):
         self.bounds = bounds
         self.genes = genes
         self.prob = prob
         self.qprob = qprob
 
         self.real_genes = self.decode(self.genes)
-        self.fitness = self.objective_function(self.real_genes)
-  
-    def decode(self,genes):
+        x, y, z = self.real_genes[0], self.real_genes[1], self.real_genes[2]
+        self.fitness = objective_function(x, y, z)
+        self.scaled_fitness = 0
+
+    def decode(self, genes):
         real_chromosome = []
         for i in range(len(self.bounds)):
             integer = int("".join(char for char in genes[i]), 2)
-            real_value = self.bounds[i][0] + (integer / (2**len(self.genes[0]))) * (
+            real_value = self.bounds[i][0] + (
+                integer / (2 ** len(self.genes[0]))
+            ) * (  # self.genes[0] = bits
                 self.bounds[i][1] - self.bounds[i][0]
             )
             real_chromosome.append(real_value)
         return real_chromosome
 
-    @staticmethod
-    def objective_function(t):
-        x = t[0]
-        y = t[1]
-        z = t[2]
-        Objective_max=x**3 + y**3 + z**4 + x*y*z
-        #Objective_max = x**3-y**3-z**4+x*y*z 
-
-        return Objective_max
-
-
-
-
 
 class GeneticAlgorithm:
-    def __init__(self, size, bits, bounds, pm, pc, cp ):
+    def __init__(self, size, bits, bounds, pm, pc, cp, objective_function):
+        self.objective_function = objective_function
         self.size = size
         self.bits = bits
         self.bounds = bounds
-        self.pm = pm 
+        self.pm = pm
         self.pc = pc
         self.cp = cp
-        self.population = [Chromosome([[str(random.randint(0,1)) for _ in range(bits)] for _ in range(3)],self.bounds) for _ in range(self.size)]
-    
-            
+        #create a population of random chromosomes
+        self.population = [
+            Chromosome(
+                [
+                    [str(random.randint(0, 1)) for _ in range(bits)]
+                    for _ in range(len(self.bounds)) #len(self.bounds) = number of variables
+                ],
+                self.bounds,
+                self.objective_function,
+            )
+            for _ in range(self.size)
+        ]
 
-
-    #calculates fitness scores/qprob
+    # calculates fitness scores/qprob
     def misc(self):
+        
         self.fitness_sum = sum([chrome.fitness for chrome in self.population])
         self.fitness_average = self.fitness_sum / len(self.population)
-        self.qprob=0
+        self.qprob = 0
         for chromosome in self.population:
             chromosome.prob = chromosome.fitness / self.fitness_sum
-            self.qprob += chromosome.prob 
+            self.qprob += chromosome.prob
             chromosome.qprob += self.qprob
-         
+
     def roulette_selection(self):
         t = []
         for _ in range(self.size):
@@ -83,8 +80,12 @@ class GeneticAlgorithm:
     def tournament_selection(self):
         selected = []
         for _ in range(self.size):
-            selected.append(max(random.sample(self.population,2), key=attrgetter('fitness')))
-        self.population = selected 
+            selected.append(
+                max(
+                    random.sample(self.population, 2), key=attrgetter("fitness")
+                )
+            )
+        self.population = selected
 
     def crossover(self):
         pop = self.population
@@ -93,13 +94,23 @@ class GeneticAlgorithm:
             parent1 = pop[2 * i - 1].genes
             parent2 = pop[2 * i].genes
             if random.random() <= self.pc:
-                child1, child2 = self.multiple_crossover(parent1, parent2, self.cp)
-                newpop.extend([Chromosome(child1,self.bounds),Chromosome(child2,self.bounds)])
-                
+                child1, child2 = self.multiple_crossover(
+                    parent1, parent2, self.cp
+                )
+                newpop.extend(
+                    [
+                        Chromosome(
+                            child1, self.bounds, self.objective_function
+                        ),
+                        Chromosome(
+                            child2, self.bounds, self.objective_function
+                        ),
+                    ]
+                )
 
             else:
-                newpop.extend([pop[2 * i - 1],pop[2*i]])
-                
+                newpop.extend([pop[2 * i - 1], pop[2 * i]])
+
         self.population = newpop
 
     def mutation(self):
@@ -116,30 +127,38 @@ class GeneticAlgorithm:
                     else:
                         z[i][j] = "1"
                     dummy.append(z[i])
-                offsprings.append(Chromosome(dummy,self.bounds))
+                offsprings.append(
+                    Chromosome(dummy, self.bounds, self.objective_function)
+                )
             else:
-                offsprings.append(Chromosome(z,self.bounds))
+                offsprings.append(
+                    Chromosome(z, self.bounds, self.objective_function)
+                )
         self.population = offsprings
 
     def __str__(self):
         s = ""
         for chrome in self.population:
 
-            s += f"{chrome.real_genes} {chrome.get_bin()} fitness ={chrome.fitness}, probability = {chrome.qprob} \n"
+            s += f"{chrome.real_genes} fitness ={chrome.fitness}, probability = {chrome.qprob} \n"
         return s
 
     def best(self):
-        return max(self.population,key=attrgetter('fitness'))
-
-
+        return max(self.population, key=attrgetter("fitness"))
 
     @staticmethod
     def multiple_crossover(parent1, parent2, cp):
         bits = len(parent1[0])
         if cp == 1:
             index = random.randint(1, bits - 1)
-            child1 = [parent1[i][:index] + parent2[i][index:] for i in range(len(parent1))]
-            child2 = [parent2[i][:index] + parent1[i][index:] for i in range(len(parent1))]
+            child1 = [
+                parent1[i][:index] + parent2[i][index:]
+                for i in range(len(parent1))
+            ]
+            child2 = [
+                parent2[i][:index] + parent1[i][index:]
+                for i in range(len(parent1))
+            ]
             return child1, child2
 
         cp = random.sample(range(1, bits), cp)
@@ -165,21 +184,6 @@ class GeneticAlgorithm:
 
     def run(self):
         self.misc()
-        self.tournament_selection()
+        self.roulette_selection()
         self.crossover()
         self.mutation()
-        
-        
-    
-
-
-
-#ga = GeneticAlgorithm(100,40,[[0, 10], [0, 20], [0, 30]],1,1,1)
-#
-#ga.run()
-#print(ga.fitness_sum)
-#
-#for i in range(100):
-#    ga.run()
-#
-#print(ga.fitness_average)
--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from itertools import count
 import tkinter as tk
-from tkinter import ttk
+from tkinter import END, ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from gnt import *
@@ -36,7 +36,7 @@ class MainWindow:
         '''
         function_label = population_label = tk.Label(
             self.root,
-            text="Συνάρτηση τριών μεταβλητών f(x, y, z)",
+            text="Συνάρτηση τριών μεταβλητών f(x, y, z) =",
             fg="#000000",
             font="Courier 14 bold",
             bg=self.color,
@@ -101,7 +101,7 @@ class MainWindow:
         '''generations slider'''
         self.generation_slider = tk.Scale(
             root,
-            from_=2,
+            from_=1,
             to=1000,
             resolution=2,
             orient="horizontal",
@@ -140,11 +140,28 @@ class MainWindow:
             column=1,
         )
 
+        
+
+        '''bits slider'''
+        self.bits_slider = tk.Scale(
+            root,
+            from_=2,
+            to=40,
+            resolution=1,
+            orient="horizontal",
+            command=self.update_scale
+        )
+        
+        self.bits_slider.grid(
+            row=6,
+            column=1,
+        )
+
         '''cp slider'''
         self.cp_slider = tk.Scale(
             root,
             from_=1,
-            to=38,
+            to=self.bits_slider.get(),
             resolution=1,
             orient="horizontal",
             
@@ -155,25 +172,14 @@ class MainWindow:
             row=5,
             column=1,
         )
-
-        '''bits slider'''
-        self.bits_slider = tk.Scale(
-            root,
-            from_=2,
-            to=40,
-            resolution=1,
-            orient="horizontal",
-        )
-        
-        self.bits_slider.grid(
-            row=6,
-            column=1,
-        )
-
-        '''function entry'''
+        '''
+        entries
+        '''
         self.function_entry = tk.Entry(self.root, width=35, font='Courier')
+        self.function_entry.insert(END, "x**2 +y**3 + z**4 + x*y*z")
         self.function_entry.grid(row=0, column=1, sticky=tk.E)
         
+        #self.test_entry = tk.Entry(self.root,width=5).grid(row=2, column=2, sticky=tk.W)
 
         '''
         BUTTONS
@@ -209,35 +215,39 @@ class MainWindow:
             self.fig,
             master=self.root,
         )
-        self.canvas.get_tk_widget().grid(row=7, column=0,sticky=tk.W)
+        self.canvas.get_tk_widget().grid(row=7, column=0, columnspan=2)
         
         
         self.axes = self.fig.add_subplot(111)
         self.root.mainloop()
 
+    def update_scale(self, new_max):
+        self.cp_slider.configure(to=int(new_max)-1)
+    
     def run(self):
         
-        
+        objective_function = self.function_entry.get()
         generations = self.generation_slider.get()
-        ga = GeneticAlgorithm(generations, self.bits_slider.get(), BOUNDS, self.pm_slider.get(), self.pc_slider.get(), self.cp_slider.get())
+        ga = GeneticAlgorithm(self.pop_slider.get(), self.bits_slider.get(), BOUNDS, self.pm_slider.get(), self.pc_slider.get(), self.cp_slider.get(), eval('lambda x,y,z:'+objective_function))
         
-        self.ys = []
-        self.zs = []
-
+        b = []
+        a = []
         for _ in range(generations):
             ga.run()
-            self.ys.append(ga.best().fitness)
-            self.zs.append(ga.fitness_average)
+            b.append(ga.best().fitness)
             
+            a.append(ga.fitness_average)
+        print(ga)
+        print(b)
         self.fig.clear()
         self.axes = self.fig.add_subplot(111)
         
-        self.axes.plot(self.ys,'r', label='max fitness')
-        self.axes.plot(self.zs,'g', label='average fitness')
+        self.axes.plot(b,'r', label='max fitness')
+        self.axes.plot(a,'g', label='average fitness')
         self.axes.set_ylabel('fitness')
         self.axes.set_xlabel('generations')
         self.axes.yaxis.set_label_position("right")
-        print(self.function_entry.get())
+        
         #ypomnima
         self.axes.legend(bbox_to_anchor=(0.5, 1.1), loc='upper center', ncol=2, fancybox=True, shadow=True)
         #forces integer spacing between generations 
